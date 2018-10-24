796eb518ffd90585d998405e2223a865a30218af
Jörg <oerteljoerg@gmail.com>
2018-10-24 09:58:28 +0200

    Merge origin/master
    Conflicts:
    	src/main/java/connectToDatabase/DBMapperOne.java

---
+++ src/main/java/DBAccess/Connector.java
@@ -0,0 +1,32 @@
+package DBAccess;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ The purpose of Connector is to...
+
+ @author kasper
+ */
+public class Connector {
+
+    private static final String URL = "jdbc:mysql://46.101.253.149:3306/useradmin";
+    private static final String USERNAME = "doorkeeper";
+    private static final String PASSWORD = "bank3*andyouarein";
+
+    private static Connection singleton;
+
+    public static void setConnection( Connection con ) {
+        singleton = con;
+    }
+
+    public static Connection connection() throws ClassNotFoundException, SQLException {
+        if ( singleton == null ) {
+            Class.forName( "com.mysql.cj.jdbc.Driver" );
+            singleton = DriverManager.getConnection( URL, USERNAME, PASSWORD );
+        }
+        return singleton;
+    }
+
+}
